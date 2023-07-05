3ddf4f31a2e88185e6efb777ba0b29e3998f3dae
yuvrajsah22kgpian <117197389+yuvrajsah22kgpian@users.noreply.github.com>
2023-07-05 10:49:00 +0530

    Add files via upload

---
+++ memories_project/Memories/server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+const CONNECTION_URL='mongodb://Indian123:Indian123@cluster0-shard-00-00.roqun.mongodb.net:27017,cluster0-shard-00-01.roqun.mongodb.net:27017,cluster0-shard-00-02.roqun.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-3xr3yp-shard-0&authSource=admin&retryWrites=true&w=majority'
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
